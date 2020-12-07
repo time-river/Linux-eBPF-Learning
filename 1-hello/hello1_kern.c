d4e182751069ecbd0c91477aa8d6e758a6db642b
river <river@vvl.me>
2020-12-07 23:22:38 +0800

    hello: third type openat

--- 1-hello/hello1_kern.c
+++ 1-hello/hello1_kern.c
@@ -3,17 +3,7 @@
 #include <linux/bpf.h>
 #include <bpf/bpf_helpers.h>
 
-#ifdef __x86_64__
-#  define SYSCALL(SYS) "__x64_" #SYS
-#elif defined(__aarch64__)
-#  define SYSCALL(SYS) "__arm64_" #SYS
-#elif defined(__s390x__)
-#  define SYSCALL(SYS) "__s390x_" #SYS
-#else
-#  define SYSCALL(SYS)  #SYS
-#endif
-
-SEC("kprobe/" SYSCALL(sys_clone))
+SEC("kprobe/sys_clone")
 int hello(struct pt_regs *ctx) {
 	char msg[] = "Hello eBPF!";
 	bpf_trace_printk(msg, sizeof(msg));
--- 1-hello/hello2_kern.c
+++ 1-hello/hello2_kern.c
@@ -6,20 +6,10 @@
 #include <bpf/bpf_core_read.h>
 #include <linux/ptrace.h>
 
-#ifdef __x86_64__
-#  define SYSCALL(SYS) "__x64_" #SYS
-#elif defined(__aarch64__)
-#  define SYSCALL(SYS) "__arm64_" #SYS
-#elif defined(__s390x__)
-#  define SYSCALL(SYS) "__s390x_" #SYS
-#else
-#  define SYSCALL(SYS)  #SYS
-#endif
-
-SEC("kprobe/" SYSCALL(sys_openat))
+SEC("kprobe/sys_openat")
 int hello(struct pt_regs *ctx) {
-	struct pt_regs *real_regs = (struct pt_regs *)PT_REGS_PARM1_CORE(ctx);
 	char fmt[] = "@dirfd='%d' @pathname='%s' @flags=0x%x";
+	struct pt_regs *real_regs = (struct pt_regs *)PT_REGS_PARM1_CORE(ctx);
 	int dirfd = PT_REGS_PARM1_CORE(real_regs);
 	char *pathname = (char *)PT_REGS_PARM2_CORE(real_regs);
 	unsigned long flags = PT_REGS_PARM3_CORE(real_regs);
