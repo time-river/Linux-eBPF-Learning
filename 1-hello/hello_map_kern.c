926ad9e5c023964473d51e55badd4f55b5c7b16d
river <river@vvl.me>
2020-12-19 00:20:05 +0800

    hello: add map

---
+++ 1-hello/hello_map_kern.c
@@ -0,0 +1,52 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
+
+#ifndef memcpy
+# define memcpy(dest, src, n)   __builtin_memcpy((dest), (src), (n))
+#endif
+
+#define MAX_LENGTH	16
+#define MAX_ENTRIES	16
+
+struct msg_t {
+	unsigned int tgid;
+	unsigned int pid;
+	char comm[MAX_LENGTH];
+	char msg[MAX_LENGTH];
+};
+
+struct bpf_map_def SEC("maps") map = {
+	.type = BPF_MAP_TYPE_HASH,
+	.key_size = sizeof(int),
+	.value_size = sizeof(struct msg_t),
+	.max_entries = MAX_ENTRIES,
+};
+
+SEC("kprobe/sys_clone")
+int hello(struct pt_regs *ctx) {
+	int key = bpf_get_smp_processor_id() % MAX_ENTRIES;
+	unsigned long cts = bpf_ktime_get_ns();
+	unsigned long id = bpf_get_current_pid_tgid();
+	struct msg_t *val, init_val;
+	char msg[MAX_LENGTH] = "Hello eBPF!";
+
+	val = bpf_map_lookup_elem(&map, &key);
+	if (val) {
+		val->tgid = (id >> 32) & 0xffffffff;
+		val->pid = id & 0xffffffff;
+		bpf_get_current_comm(val->comm, sizeof(val->comm));
+		memcpy(val->msg, msg, MAX_LENGTH);
+	} else {
+		init_val.tgid = (id >> 32) & 0xffffffff;
+		init_val.pid = id & 0xffffffff;
+		bpf_get_current_comm(init_val.comm, sizeof(init_val.comm));
+		memcpy(init_val.msg, msg, MAX_LENGTH);
+		bpf_map_update_elem(&map, &key, &init_val, BPF_NOEXIST);
+	}
+
+	return 0;
+}
+
+char _license[] SEC("license") = "GPL";
---
+++ 1-hello/hello_map_user.c
@@ -0,0 +1,51 @@
+#include <bpf/bpf.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+#include <errno.h>
+
+#include "../lib/bpf_load.h"
+
+#define MAX_FILENAME_LEN 256
+
+#define MAX_LENGTH	16
+#define MAX_ENTRIES	16
+
+struct msg_t {
+	unsigned int tgid;
+	unsigned int pid;
+	char comm[MAX_LENGTH];
+	char msg[MAX_LENGTH];
+};
+
+int main(int argc, char *argv[]) {
+	struct msg_t msg = {0};
+
+	if (argc != 2) {
+		fprintf(stdout, "Usage: %s <eBPF program>\n", argv[0]);
+		return 0;
+	}
+
+	if (access(argv[1], R_OK) != 0) {
+		fprintf(stderr, "ERROR: access('%s'): %s\n",
+			argv[0], strerror(errno));
+		return 1;
+	}
+
+	if (load_bpf_file(argv[1])) {
+		fprintf(stdout, "%s", bpf_log_buf);
+		return 1;
+	}
+
+	for (int key = 0; ; key = (key+1)%MAX_ENTRIES) {
+		if (!bpf_map_lookup_elem(map_fd[0], &key, &msg)) {
+			fprintf(stdout, "@tgid='%u' @gid='%u' @comm='%s' @msg='%s'\n",
+				msg.tgid, msg.pid, msg.comm, msg.msg);
+			memset(&msg, 0, sizeof(msg));
+			bpf_map_delete_elem(map_fd[0], &key);
+		}
+	}
+
+	return 0;
+}
