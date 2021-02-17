f73c14d4d424a7dc286980668588ac895b519064
river <river@vvl.me>
2021-02-17 15:02:42 +0800

    4-CO-RE: init

---
+++ 4-CO-RE/connect.c
@@ -0,0 +1,95 @@
+// The tracepoint requires kernel CONFIG_FTRACE_SYSCALLS to be set.
+
+#include <stdio.h>
+#include <string.h>
+#include <signal.h>
+#include <errno.h>
+#include <sys/resource.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include "connect_kern.skel.h"
+
+#define MAX_LENGTH 16
+
+struct msg {
+        char comm[MAX_LENGTH];
+	struct sockaddr uservaddr;
+};
+
+static struct connect_kern *skel;
+static struct ring_buffer *rb;
+static bool stop;
+
+static void sigint_handler(int sig) {
+	stop = true;
+}
+
+static int print_bpf_output(void *ctx, void *data, size_t size) {
+	struct msg *val = data;
+	struct sockaddr_in *dest = (struct sockaddr_in *)&val->uservaddr;
+
+	fprintf(stdout, ">> [%s]: connect '%s:%d'\n",
+			val->comm,
+			inet_ntoa(dest->sin_addr),
+			dest->sin_port);
+	return 0;
+}
+
+/* Prevent:
+ *   libbpf: load bpf program failed: Operation not permitted
+ *   libbpf: permission error while running as root; try raising 'ulimit -l'?
+ *   current value: 64.0 KiB
+ *   libbpf: failed to load program 'tracepoint/syscalls/sys_enter_openat'
+ */
+void setlimit(void) {
+	struct rlimit r = {RLIM_INFINITY, RLIM_INFINITY};
+	setrlimit(RLIMIT_MEMLOCK, &r);
+}
+
+int main(int argc, char *argv[]) {
+	struct ring_buffer_opts rb_opts = {};
+	int retval = 0;
+	int map_fd;
+
+	setlimit();
+
+	skel = connect_kern__open_and_load();
+	if (!skel)
+		goto cleanup;
+
+	map_fd = bpf_object__find_map_fd_by_name(skel->obj, "ringbuf");
+	if (map_fd < 0) {
+		fprintf(stderr, "ERROR: finding a map in obj file failed!\n");
+		goto cleanup;
+	}
+
+	retval = connect_kern__attach(skel);
+	if (retval)
+		goto cleanup;
+
+	if (signal(SIGINT, sigint_handler) == SIG_ERR) {
+		fprintf(stderr, "ERROR: %s\n", strerror(errno));
+		goto cleanup;
+	}
+
+	rb = ring_buffer__new(map_fd, print_bpf_output, NULL, NULL);
+	if (rb == NULL) {
+		fprintf(stderr, "ERROR: failed to setup bpf ring_buffer!\n");
+		goto cleanup;
+	}
+
+	while (true) {
+		ring_buffer__poll(rb, 1000);
+		if (stop)
+			break;
+	}
+
+	connect_kern__detach(skel);
+	ring_buffer__free(rb);
+	bpf_link__destroy(skel->links.hello);
+cleanup:
+	connect_kern__destroy(skel);
+	return 0;
+}
---
+++ 4-CO-RE/connect_kern.c
@@ -0,0 +1,55 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+// TODO: call unknown#195896080 when enabling CO-RE
+#define BPF_NO_PRESERVE_ACCESS_INDEX
+#include "vmlinux.h"
+
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_core_read.h>
+#include <bpf/bpf_tracing.h>
+
+#ifndef INT_MAX
+# define INT_MAX	((1l << 32) - 1)
+#endif
+
+struct syscalls_enter_sendmsg_args {
+	__u64 __pad[2];
+	__u64 fd;
+	__u64 uservaddr;
+	__u64 addrlen;
+};
+
+#define MAX_LENGTH 16
+
+struct msg {
+	char comm[MAX_LENGTH];
+	struct sockaddr uservaddr;
+};
+
+struct {
+	__uint(type, BPF_MAP_TYPE_RINGBUF);
+	__uint(max_entries, IOC_PAGE_SIZE); // need page align
+} ringbuf SEC(".maps");
+
+static int id = -1;
+
+//SEC("tracepoint/syscalls/sys_enter_openat")
+SEC("tp/syscalls/sys_enter_connect")
+int hello(struct syscalls_enter_sendmsg_args *ctx) {
+	struct msg *val;
+
+	val = bpf_ringbuf_reserve(&ringbuf, sizeof(*val), 0);
+	if (!val)
+		goto out;
+
+	bpf_get_current_comm(val->comm, sizeof(val->comm));
+	bpf_probe_read_user(&val->uservaddr,
+			    sizeof(val->uservaddr), (void *)ctx->uservaddr);
+
+	bpf_ringbuf_output(&ringbuf, val, sizeof(*val), 0);
+	bpf_ringbuf_discard(val, 0);
+out:
+	return 0;
+}
+
+char _license[] SEC("license") = "GPL";
